f7405cb2a50543a65e287d87011eef32517a0856
Anirudh Dabral <anirudhdabral49@gmail.com>
2023-12-15 11:41:19 +0530

    first commit

---
+++ src/main/java/com/ngt/ep3/NgtEp3Application.java
@@ -0,0 +1,13 @@
+package com.ngt.ep3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgtEp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgtEp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngt/ep3/NgtEp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngt.ep3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgtEp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
